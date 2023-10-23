34319ddd292ffe29b4e6b0e5b62b9058f436dc52
Troy Chaplin <troy.chaplin@gmail.com>
2023-10-23 10:46:12 -0400

    change: cleaned up field wrapper component

--- src/components/FieldWrapper/FieldWrapper.tsx
+++ src/components/FieldWrapper/FieldWrapper.tsx
@@ -6,9 +6,8 @@ interface FieldWrapperProps {
 }
 
 export const FieldWrapper = ({ children, cols }: FieldWrapperProps) => {
-  const gridSpacing = cols ? gridColsClass[cols] : 'space-y-5'
-  console.log(gridSpacing)
+  const gridSpacing = cols ? gridColsClass[cols] : "space-y-5";
   return <div className={`grid ${gridSpacing} gap-5`}>{children}</div>;
-}
+};
 
 export default FieldWrapper;
